acc8319fff491ab707adfae5147b99b5f1a6f36d
ardiandev-offc <ardiandevotp@gmail.com>
2024-08-27 17:00:06 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://api.ardiandev.me';
+var url = 'https://api.ardiandev.ardianstore.biz.id';
 try {
-if(url == 'https://api.ardiandev.me') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://api.ardiandev.ardianstore.biz.id') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
